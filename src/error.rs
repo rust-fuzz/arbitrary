2b8aca6d810b313aa2bf2d3529684488db595c35
Nick Fitzgerald <fitzgen@gmail.com>
2019-12-26 15:29:39 -0800

    Implement `std::error::Error` for `arbitrary::Error`

--- src/error.rs
+++ src/error.rs
@@ -1,14 +1,26 @@
+use std::{error, fmt};
+
 /// An enumeration of buffer creation errors
 #[derive(Debug, Clone, Copy)]
 #[non_exhaustive]
 pub enum Error {
-    /// The input buffer is empty, causing construction of some buffer types to
-    /// fail
-    EmptyInput,
-
-    /// There is not enough underlying data to fulfill a request for raw bytes.
+    /// There was not enough underlying data to fulfill some request for raw
+    /// bytes.
     NotEnoughData,
 }
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Error::NotEnoughData => write!(
+                f,
+                "There is not enough underlying raw data to construct an `Arbitrary` instance"
+            ),
+        }
+    }
+}
+
+impl error::Error for Error {}
 
 /// A `Result` with the error type fixed as `arbitrary::Error`.
 ///
