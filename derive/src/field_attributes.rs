479da322a5b65917bb933dc43b03a83ecdec0c03
Sergey Potapov <blake131313@gmail.com>
2022-10-20 17:17:51 +0200

    Get rid of the rest of panics within determine_field_constructor()

--- derive/src/field_attributes.rs
+++ derive/src/field_attributes.rs
@@ -1,8 +1,8 @@
 use proc_macro2::{Group, Span, TokenStream, TokenTree};
 use quote::quote;
-use syn::*;
+use syn::{spanned::Spanned, *};
 
-/// Used to filter out necessary field attribute and in panics.
+/// Used to filter out necessary field attribute and within error messages.
 static ARBITRARY_ATTRIBUTE_NAME: &str = "arbitrary";
 
 /// Determines how a value for a field should be constructed.
@@ -42,9 +42,10 @@ fn fetch_attr_from_field(field: &Field) -> Result<Option<&Attribute>> {
         .collect();
     if found_attributes.len() > 1 {
         let name = field.ident.as_ref().unwrap();
-        panic!(
+        let msg = format!(
             "Multiple conflicting #[{ARBITRARY_ATTRIBUTE_NAME}] attributes found on field `{name}`"
         );
+        return Err(syn::Error::new(field.span(), msg));
     }
     Ok(found_attributes.into_iter().next())
 }
@@ -52,12 +53,16 @@ fn fetch_attr_from_field(field: &Field) -> Result<Option<&Attribute>> {
 fn parse_attribute(attr: &Attribute) -> Result<FieldConstructor> {
     let group = {
         let mut tokens_iter = attr.clone().tokens.into_iter();
-        let token = tokens_iter
-            .next()
-            .unwrap_or_else(|| panic!("#[{ARBITRARY_ATTRIBUTE_NAME}] cannot be empty."));
+        let token = tokens_iter.next().ok_or_else(|| {
+            let msg = format!("#[{ARBITRARY_ATTRIBUTE_NAME}] cannot be empty.");
+            syn::Error::new(attr.span(), msg)
+        })?;
         match token {
             TokenTree::Group(g) => g,
-            t => panic!("#[{ARBITRARY_ATTRIBUTE_NAME}] must contain a group, got: {t})"),
+            t => {
+                let msg = format!("#[{ARBITRARY_ATTRIBUTE_NAME}] must contain a group, got: {t})");
+                return Err(syn::Error::new(attr.span(), msg));
+            }
         }
     };
     parse_attribute_internals(group)
@@ -66,9 +71,10 @@ fn parse_attribute(attr: &Attribute) -> Result<FieldConstructor> {
 fn parse_attribute_internals(group: Group) -> Result<FieldConstructor> {
     let stream = group.stream();
     let mut tokens_iter = stream.into_iter();
-    let token = tokens_iter
-        .next()
-        .unwrap_or_else(|| panic!("#[{ARBITRARY_ATTRIBUTE_NAME}] cannot be empty."));
+    let token = tokens_iter.next().ok_or_else(|| {
+        let msg = format!("#[{ARBITRARY_ATTRIBUTE_NAME}] cannot be empty.");
+        syn::Error::new(group.span(), msg)
+    })?;
     match token.to_string().as_ref() {
         "default" => Ok(FieldConstructor::Default),
         "with" => {
