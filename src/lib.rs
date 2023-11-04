e5f3d2ae4a2a44b4f426d1851cd1e5e3940c0373
Carlos López <00xc@protonmail.com>
2023-11-04 15:17:12 +0100

    Remove needess checks in CString::arbitrary()
    Use CString::from_vec_unchecked() to get a new CString, as all zero
    bytes have been removed, improving performance. In debug builds, this
    function will still check if any zero bytes are present.

--- src/lib.rs
+++ src/lib.rs
@@ -885,7 +885,8 @@ impl<'a> Arbitrary<'a> for CString {
     fn arbitrary(u: &mut Unstructured<'a>) -> Result<Self> {
         <Vec<u8> as Arbitrary>::arbitrary(u).map(|mut x| {
             x.retain(|&c| c != 0);
-            Self::new(x).unwrap()
+            // SAFETY: all zero bytes have been removed
+            unsafe { Self::from_vec_unchecked(x) }
         })
     }
 
